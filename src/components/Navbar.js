c6e30ff466e72c6aa62b3c21bcc05d9b195f68e1
Pradeep Raut <rautpradeep10@yahoo.com>
2021-05-03 17:12:28 +0545

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { menuData } from '../data/MenuData';
+import styled from 'styled-components';
 
+const Menubar = styled.i`
+display : none`;
 
 const Navbar = () => {
+    
+    const ManuItems = document.getElementById("MenuItems");
+
+    // ManuItems.style.maxHeight = "0px";
+
+    const menutoggle = () => {
+        if(ManuItems.style.maxHeight === "0px"){
+            ManuItems.style.maxHeight="200px";
+        }
+        else{
+            ManuItems.style.maxHeight="0px";
+        }
+    }
+
     return (
         <>
         <div className="navbar">
              <div className="logo">
                  <img src="images/logo.png" width="125px" alt="logo"/>
              </div>
+             <Menubar />
              <nav>
                  <ul id="MenuItems">
                      {menuData.map((item, index)=>(
@@ -16,8 +34,9 @@ const Navbar = () => {
                      ))}
                  </ul>
              </nav>
+             <img src="images/Hamburger_icon.svg.png" className="menu-icon" onClick={menutoggle}/>
          </div>
-             
+         
         </>
     )
 }
